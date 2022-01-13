1925eeb12fa3b817e330abf1d9f0b0096b7e7f3d
lihong <1091262281@qq.com>
2022-01-13 22:25:00 +0800

    :sparkles:  init

---
+++ src/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const Thing = () => {
+  return <h1>edit-ag-grid</h1>;
+};
---
+++ tsdx.config.js
@@ -0,0 +1,33 @@
+const postcss = require('rollup-plugin-postcss');
+module.exports = {
+  rollup(config, options) {
+    const _external = config.external;
+    config.external = (id) => {
+      /** 配置需要打包的依赖 */
+      if (['lodash-es'].includes(id)) {
+        return false;
+      }
+      else {
+        return _external(id);
+      }
+      
+    }
+    config.plugins.push(
+      postcss({
+        inject: true,
+        extract: !!options.writeMeta,
+        // modules: true, // 使用css modules
+        // namedExport: true, // 类名导出
+        camelCase: true, // 支持驼峰
+        // sass: true, // 是否使用sass
+        less:true,
+        // autoModules:true,
+        // namedExports(name) {
+        //   // Maybe you simply want to convert dash to underscore
+        //   return name.replace(/-/g, '_')
+        // }
+      })
+    );
+    return config;
+  },
+};
